fc0bd7ab84e58a12998aa924413b3f34640c8fb4
junrdev <juniorprogrammer09@gmail.com>
2023-07-29 01:07:33 +0300

    commandline runner

--- src/main/java/com/junrdev/librarysystem/LibrarysystemApplication.java
+++ src/main/java/com/junrdev/librarysystem/LibrarysystemApplication.java
@@ -1,13 +1,34 @@
 package com.junrdev.librarysystem;
 
+import com.junrdev.librarysystem.dto.BookDTO;
+import com.junrdev.librarysystem.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
-public class LibrarysystemApplication {
+public class LibrarysystemApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibrarysystemApplication.class, args);
-	}
+    @Autowired
+    private BookService service;
 
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarysystemApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+//        service.addBook(BookDTO.builder().
+//                title("Hello java")
+//                .author("Brian")
+//                .category("Java")
+//                .coverPhoto("google.com")
+//                .isbn(UUID.randomUUID().toString())
+//                .noOfPages(200)
+//                .build()
+//        );
+    }
 }
